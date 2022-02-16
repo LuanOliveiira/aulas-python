ec51650f3c1a305439f9d11119815bd4a3b0b828
LuanOliveira <luanoliveiraterra@gmail.com>
2022-02-16 14:02:58 -0300

    Aula1

---
+++ listas-tuplas.py
@@ -0,0 +1,12 @@
+lista_capitais = []
+lista_capitais.append('Brasília')
+lista_capitais.append('Buenos Aires')
+lista_capitais.append('Pequim')
+lista_capitais.append('Bogotá')
+
+print(lista_capitais)
+
+lista_capitais.remove('Buenos Aires')
+
+lista_capitais.pop(2)
+
---
+++ tuplas.py
@@ -0,0 +1,14 @@
+nomes_paises = ('Brasil', 'Argentina', 'China', 'Canadá', 'Japão')
+print(nomes_paises)
+
+nome_estado = 'São Paulo',
+print(nome_estado, type(nome_estado))
+
+len(nomes_paises)
+
+nomes_paises[0]
+
+b, a, c, ca, j = nomes_paises
+print(b, c, j)
+
+print(*nomes_paises)
